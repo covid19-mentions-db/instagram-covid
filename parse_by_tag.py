d0171d0b7c432a7c88924174730969ef461e7cbd
covid <dev@covid19-mentions-db.com>
2020-04-14 00:54:34 +0500

    increase sleep time

--- parse_by_tag.py
+++ parse_by_tag.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     print('start parsing for tag', _TAG)
     parse_by_tag(_TAG, write_callback=result_write_callback)
 
-    print('task is completed' 'sleep(6000)')
-    sleep(6000)
+    print('task is completed' 'sleep(60000)')
+    sleep(60000)
